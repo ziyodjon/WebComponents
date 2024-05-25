f5630261416a25160473bccece84ad49a56d0c26
ziyodjon <ziyodjon@gmail.com>
2024-05-25 20:53:44 +0500

    LastChanges

--- classes/ProductCounter.js
+++ classes/ProductCounter.js
@@ -15,9 +15,14 @@ export default class ProductCounter extends HTMLElement{
         this.minusBtn.textContent = '-';
 
         this.inptCounter = document.createElement('input');
+        this.inptCounter.type = 'text';
 
         
         this.append(this.minusBtn,this.inptCounter,this.plusBtn);
+
+        this.addEventListener('submit', (e) => {
+            alert();
+        });
 
         this.plusBtn.addEventListener('click', () => {
             this.plus();
@@ -29,23 +34,19 @@ export default class ProductCounter extends HTMLElement{
 
         if(this.counter >= parseInt(this.maxCount)){
             this.plusBtn.setAttribute('disabled',true);
-            console.log("plus" + this.counter);
         }else{
-            console.log('minus', this.counter);
-            this.plusBtn.removeAttribute('disabled') 
+            this.plusBtn.removeAttribute('disabled');
         }
-
-
-        
+ 
     }
 
 
     minus (){
         this.inptCounter.setAttribute('value',--this.counter);
         this.setAttribute('count',this.counter); 
-       if(this.counter <= 0){
-            this.minusBtn.setAttribute('disabled',true);
-       }
+    //    if(this.counter <= 0){
+    //         this.minusBtn.setAttribute('disabled',true);
+    //    }
     }
 
     plus (){
@@ -54,16 +55,22 @@ export default class ProductCounter extends HTMLElement{
     }
 
     attributeChangedCallback(attr, oldValue, newValue) {
+
         
         if(attr === 'count'){
 
             this.inptCounter.setAttribute('value',newValue);
+
             this.counter = newValue;
+
+            if(this.counter > this.maxCount){
+                this.plusBtn.setAttribute('disabled',true);
+            }
 
             if(newValue >= parseInt(this.maxCount)) {
                 this.plusBtn.setAttribute('disabled',true);
             } else {
-                this.plusBtn.removeAttribute('disabled')
+                this.plusBtn.removeAttribute('disabled');
             }
 
             if(newValue <= 0) {
@@ -76,9 +83,21 @@ export default class ProductCounter extends HTMLElement{
         if(attr === 'max_count'){
             this.maxCount = newValue;
 
-            if(oldValue < newValue && oldValue != null){
+            if(this.counter > this.maxCount){
+                this.plusBtn.setAttribute('disabled',true);
+            }
+
+            // if(this.maxCount < this.counter){
+            //     console.log('Max 3');
+            // }
+
+            if(this.counter < this.maxCount){
                 this.plusBtn.removeAttribute('disabled');
             }
+
+            // if(oldValue < newValue && oldValue != null){
+            //     //this.plusBtn.removeAttribute('disabled');
+            // }
         }
     }
 
